e38f82cf9cb9b8d9e14abc5e82adc82059b71009
ahmed7100 <ahmedsalim7100@gmail.com>
2023-04-24 21:21:06 -0700

    SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,14 @@
 # Puppet manifest to configure ssh client with public key authentication
+include stdlib
+
 file_line { 'Turn off password authentication':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthetication no',
+  line   => '    PasswordAuthetication no',
 }
 
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'Identityfile ~/.ssh/school',
+  line   => '     Identityfile ~/.ssh/school',
 } 
